779970a30d0411998743fda0c7e3790c9bf45fc0
Bartłomiej Jacak <bartek.jacak@gmail.com>
2015-05-07 19:18:51 +0200

    HTML and styles of projects and contact sections.
    * Added HTML to index.html
    * Added styles to style.css
    * When you click arrow page scrolls to bio section
    * Twitter, facebook and google plus hovers
    * Added arrow bounce animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
+	// $(".").hover(function() {
+	//    $(this).siblings('p').stop().animate({
+	//    	opacity: "1"
+	//    },200, function(){
+	//    	$(this).css('display','block');
+	//    });
+
+	//    $(this).siblings('.').stop().animate({
+	//    	height: "100%"
+	//    },200, function(){
+	//    	$('.').css('height','100%');
+	//    });
+	// });
+
+	// $('.').mouseleave(function() {
+	//    $(this).find("p").animate({
+	//    	opacity: "0"
+	//    },200, function(){
+	//    	$(this).css('display','none');
+	//    });
+
+	//    $('.').animate({
+	//    	height: "60px"
+	//    },200, function(){
+	//    	$('.').css('height','60px');
+	//    });
+
+	// });
+
+	$('#arrow').click(function(){
+		$('html, body').animate({
+		    scrollTop: $('#arrow-box').offset().top
+		}, 1000);
+		return false;
+	});
 
 });
