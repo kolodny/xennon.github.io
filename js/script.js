6c82906ad9aa1da235e6051848d52242377c52b1
Bartłomiej Jacak <bartek.jacak@gmail.com>
2015-05-09 01:34:32 +0200

    Animations.
    * Moved animations to animations.css
    * Scroll magic library
    * Animations

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
 $(document).ready(function() {
+	var controller = new ScrollMagic.Controller();
 
-	  var skills = new Array();
-		  skills[0] = 65; //html/css
-		  skills[1] = 45; //sass
-		  skills[2] = 40; //js/jquery
-		  skills[3] = 40; //angular
-		  skills[4] = 65; //illustrator
-		  skills[5] = 55; //git
-		  skills[6] = 40; //c++
-		  skills[7] = 40; //arduino
+	var skills = new ScrollMagic.Scene({triggerElement: "#skills", duration: 500})
+	.addTo(controller).on("enter leave", function (e) {
+			progressbarAnimation();
+		});
 
-	for(var i=0; i<8; i++) {
-		$('.fill:eq(' + i +')').animate({
-		    width: skills[i] + '%'
-		}, 1000);
-	};
+	var projects = new ScrollMagic.Scene({triggerElement: "#projects", duration: 100, offset: -100})
+	.addTo(controller).on("enter leave", function (e) {
+			$('#projects li').removeClass('hidden');
+			$('#projects li:nth-child(odd)').addClass('animated fadeInLeft');
+			$('#projects li:nth-child(even)').addClass('animated fadeInRight');
+		});
+
+
+	var contact = new ScrollMagic.Scene({triggerElement: "#contact", duration: 100, offset: -100})
+	.addTo(controller).on("enter leave", function (e) {
+			$('#contact form').removeClass('hidden');
+			$('#contact form').addClass('animated fadeInUp');
+		});
+
+
+  var skills = new Array();
+	  skills[0] = 65; //html/css
+	  skills[1] = 45; //sass
+	  skills[2] = 40; //js/jquery
+	  skills[3] = 40; //angular
+	  skills[4] = 65; //illustrator
+	  skills[5] = 55; //git
+	  skills[6] = 40; //c++
+	  skills[7] = 40; //arduino
+
+	function progressbarAnimation(){
+		for(var i=0; i<8; i++) {
+				$('.fill:eq(' + i +')').animate({
+			    width: skills[i] + '%'
+					}, 1000);
+		};
+	}
 
 	$('#arrow').click(function(){
 		$('html, body').animate({
