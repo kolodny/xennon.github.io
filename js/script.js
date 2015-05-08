6e0e6d0ecde208efd5dadef51e2eec3362879c69
Bartłomiej Jacak <bartek.jacak@gmail.com>
2015-05-09 00:03:44 +0200

    Small fixes.
    * Changed animation speed of arrow
    * Changed background image of contact section
    * Better footer
    * Animation of every project box
    * Deleted gradient from progress bars
    * Animated progress bars

--- js/script.js
+++ js/script.js
@@ -1,36 +1,24 @@
 $(document).ready(function() {
-	// $(".").hover(function() {
-	//    $(this).siblings('p').stop().animate({
-	//    	opacity: "1"
-	//    },200, function(){
-	//    	$(this).css('display','block');
-	//    });
 
-	//    $(this).siblings('.').stop().animate({
-	//    	height: "100%"
-	//    },200, function(){
-	//    	$('.').css('height','100%');
-	//    });
-	// });
+	  var skills = new Array();
+		  skills[0] = 65; //html/css
+		  skills[1] = 45; //sass
+		  skills[2] = 40; //js/jquery
+		  skills[3] = 40; //angular
+		  skills[4] = 65; //illustrator
+		  skills[5] = 55; //git
+		  skills[6] = 40; //c++
+		  skills[7] = 40; //arduino
 
-	// $('.').mouseleave(function() {
-	//    $(this).find("p").animate({
-	//    	opacity: "0"
-	//    },200, function(){
-	//    	$(this).css('display','none');
-	//    });
-
-	//    $('.').animate({
-	//    	height: "60px"
-	//    },200, function(){
-	//    	$('.').css('height','60px');
-	//    });
-
-	// });
+	for(var i=0; i<8; i++) {
+		$('.fill:eq(' + i +')').animate({
+		    width: skills[i] + '%'
+		}, 1000);
+	};
 
 	$('#arrow').click(function(){
 		$('html, body').animate({
-		    scrollTop: $('#arrow-box').offset().top
+		    scrollTop: $('#bio').offset().top
 		}, 1000);
 		return false;
 	});
